5b4cad74f8122cb62e7e5434d83fcca0faea109b
xuanxiao2013 <xuanxiao2013@sina.cn>
2014-12-05 13:34:09 +0800

    咬定青山不放松，立根原在破岩中。 千磨万击还坚劲，任尔东西南北风。

---
+++ reload/server.js
@@ -0,0 +1,20 @@
+var fs = require('fs'),
+    util = require('util'),
+    path = require('path'),
+    cp = require('child_process'),
+    chokidar = require('chokidar');
+
+var pdir = '/Users/xuanxiao/work/www/github/rsync-watch-file/reload/';
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+console.log('监听目录:' + pdir)
+watch = chokidar.watch(pdir, {
+    ignored: /[\/\\]\./,
+    persistent: true,
+    ignoreInitial: true
+}).on('all', function(event, path) {
+    if (path) {
+        io.sockets.emit(event);
+    }
+});
+server.listen(3000);
